efb36eac45df2df27df185576aeb4632d6c23ba9
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-11-08 20:21:53 +0600

    All Assignment Page data loaded sucessfully

--- src/components/AllAssignment/AllAssignment.jsx
+++ src/components/AllAssignment/AllAssignment.jsx
@@ -1,20 +1,57 @@
 import PropTypes from "prop-types";
 
-const AllAssignment = ({assignment}) => {
+const AllAssignment = ({ assignment }) => {
+  const {
+    _id,
+    title,
+    thumbnail,
+    assignmentDifficultyLevel,
+    marks,
+    assignmentDescription,
+    dueDate,
+    assignmentCreatorName,
+  } = assignment;
 
-    const{_id,title,thumbnail,assignmentDifficultyLevel,marks,assignmentDescription,dueDate,assignmentCreatorName} = assignment;
+  console.log(
+    _id,
+    title,
+    thumbnail,
+    assignmentDifficultyLevel,
+    marks,
+    assignmentDescription,
+    dueDate,
+    assignmentCreatorName
+  );
 
-    console.log(_id,title,thumbnail,assignmentDifficultyLevel,marks,assignmentDescription,dueDate,assignmentCreatorName)
-
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="card lg:card-side bg-base-100 shadow-xl lg:h-96">
+        <figure className="p-4 lg:w-3/4 ">
+          <img
+          className="h-full"
+            src={thumbnail}
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>Total Marks: {marks}</p>
+          <p className="badge badge-primary badge-outline w-16">{assignmentDifficultyLevel}</p>
+          <p>Created By {assignmentCreatorName}</p>
+          <p>Due Date: {dueDate}</p>
+          <div className="card-actions justify-center">
+            <button className="btn btn-info">View Assignment</button>
+            <button className="btn btn-success">Update Assignment</button>
+            <button className="btn btn-error">Delete Assignment</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 AllAssignment.propTypes = {
-    assignment: PropTypes.object,
-  };
+  assignment: PropTypes.object,
+};
 
 export default AllAssignment;
--- src/components/AllAssignments/AllAssignments.jsx
+++ src/components/AllAssignments/AllAssignments.jsx
@@ -8,7 +8,7 @@ const AllAssignments = () => {
   return (
     <div>
       <h1 className="text-center font-bold text-4xl">All Assignment Section</h1>
-      <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-4">
         {assignments.map((assignment) => (
           <AllAssignment key={assignment._id} assignment={assignment}></AllAssignment>
         ))}
